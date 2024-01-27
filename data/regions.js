419e270434f3fa0daa15dd51d6857bf291dea2cb
Juani <juaninolivillar@gmail.com>
2024-01-27 03:02:56 -0300

    push

--- data/regions.js
+++ data/regions.js
@@ -1,54 +1,80 @@
-const SAMERICA = ["SCL", "LIM", "BOG", "BUE", "MVD", "ASU", "UIO"];
+const SAMERICA = []
+//["SCL", "LIM", "BOG", "BUE", "MVD", "ASU", "UIO"];
 
-const LIMITROFE = ["SCL", "MVD", "ASU", "SAO", "RIO"];
+const LIMITROFE = []
+//["SCL", "MVD", "ASU", "SAO", "RIO"];
 
-const ARGENTINA = ["BUE", "COR", "ROS", "MDZ", "NQN", "BRC", "IGR"];
+const ARGENTINA = []
+//["BUE", "COR", "ROS", "MDZ", "NQN", "BRC", "IGR"];
 
-const BRASIL = ["RIO", "SAO", "FLN", "MCZ", "SSA", "REC", "NAT", "IGU"];
+const BRASIL = []
+//["RIO", "SAO", "FLN", "MCZ", "SSA", "REC", "NAT", "IGU"];
 
-const COLOMBIA = ["BOG", "ADZ", "CTG", "SMR"];
+const COLOMBIA = []
+//["BOG", "ADZ", "CTG", "SMR"];
 
-const CARIBE = ["CUN", "PTY", "PUJ", "SJO", "AUA", "HAV", "CTG", "SJU"];
+const CARIBE = []
+//["CUN", "PTY", "PUJ", "SJO", "AUA", "HAV", "CTG", "SJU"];
 
-const NAMERICA = ["MEX", "CHI", "NYC", "LAX", "DFW", "SFO", "LAS"];
+const NAMERICA = []
+//["MEX", "CHI", "NYC", "LAX", "DFW", "SFO", "LAS"];
 
-const USAESTE = ["NYC", "WAS", "PHL", "BOS", "DTT", "CHI"];
+const USAESTE = []
+//["NYC", "WAS", "PHL", "BOS", "DTT", "CHI"];
 
-const USAOESTE = ["LAX", "HNL", "SFO", "LAS", "SAN", "SMF"];
+const USAOESTE = []
+//["LAX", "HNL", "SFO", "LAS", "SAN", "SMF"];
 
-const USASUR = ["DFW", "PHX", "IAH", "SAT", "ATL"];
+const USASUR = []
+//["DFW", "PHX", "IAH", "SAT", "ATL"];
 
-const FLORIDA = ["MIA", "FLL", "MCO", "TPA"];
+const FLORIDA = []
+//["MIA", "FLL", "MCO", "TPA"];
 
-const HAWAII = ["HNL", "LIH", "KOA", "OGG"];
+const HAWAII = []
+//["HNL", "LIH", "KOA", "OGG"];
 
-const CANADA = ["YTO", "YMQ", "YVR", "YOW", "YQB"];
+const CANADA = []
+//["YTO", "YMQ", "YVR", "YOW", "YQB"];
 
-const EUROPA = ["LIS", "MAD", "BCN", "PAR", "AMS", "ROM", "LON", "FRA", "IST"];
+const EUROPA = []
+//["LIS", "MAD", "BCN", "PAR", "AMS", "ROM", "LON", "FRA", "IST"];
 
-const CEUROPA = ["BRU", "ATH", "BER", "ZRH", "VIE", "PRG"];
+const CEUROPA = []
+//["BRU", "ATH", "BER", "ZRH", "VIE", "PRG"];
 
-const ESPANA = ["MAD", "BCN", "VLC", "PMI", "AGP", "IBZ", "SVQ", "BIO"];
+const ESPANA = []
+//["MAD", "BCN", "VLC", "PMI", "AGP", "IBZ", "SVQ", "BIO"];
 
-const ITALIA = ["ROM", "MIL", "BLQ", "VCE", "NAP"];
+const ITALIA = []
+//["ROM", "MIL", "BLQ", "VCE", "NAP"];
 
-const FRANCIA = ["PAR", "MRS", "NCE", "LYS", "NTE", "TLS"];
+const FRANCIA = []
+//["PAR", "MRS", "NCE", "LYS", "NTE", "TLS"];
 
-const NORDICO = ["CPH", "HEL", "STO", "OSL", "BGO", "SVG", "GOT"];
+const NORDICO = []
+//["CPH", "HEL", "STO", "OSL", "BGO", "SVG", "GOT"];
 
-const ASIA = ["DXB", "BKK", "TLV", "TYO", "SEL", "DPS"];
+const ASIA = []
+//["DXB", "BKK", "TLV", "TYO", "SEL", "DPS"];
 
-const MORIENTE = ["IST", "CAI", "DXB", "TLV", "DOH"];
+const MORIENTE = []
+//["IST", "CAI", "DXB", "TLV", "DOH"];
 
-const SASIA = ["BKK", "SIN", "MLE", "DPS", "SGN", "KUL"];
+const SASIA = []
+//["BKK", "SIN", "MLE", "DPS", "SGN", "KUL"];
 
-const NASIA = ["TYO", "SEL", "HKG"];
+const NASIA = []
+//["TYO", "SEL", "HKG"];
 
-const INDIA = ["DEL", "BLR", "BOM", "CCU", "JAI"];
+const INDIA = []
+//["DEL", "BLR", "BOM", "CCU", "JAI"];
 
-const AFRICA = ["CAI", "SEZ", "CPT", "DAR", "ADD", "RBA"];
+const AFRICA = []
+//["CAI", "SEZ", "CPT", "DAR", "ADD", "RBA"];
 
-const OCEANIA = ["AKL", "SYD", "MEL"];
+const OCEANIA = []
+//["AKL", "SYD", "MEL"];
 
 const regions = {
   SAMERICA,
