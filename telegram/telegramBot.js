1b18e0ff1e848955b973bec995cb9d0c831a2f3e
Juani <juaninolivillar@gmail.com>
2024-01-28 15:41:36 -0300

    fix

--- telegram/telegramBot.js
+++ telegram/telegramBot.js
@@ -546,7 +546,7 @@ async function enqueueRequest(requestFunction, args, chat_id, bot, send_message)
 
     if (queue_size > 1) {
         let estimated_time = (queue_size - 1) * rateLimitInterval / 1000
-        estimated_time += Math.min((Math.abs(30 - (Date.now() - lastRequestTime))) / 1000, 30)
+        estimated_time += Math.min((Math.abs(intervalInSeconds - (Date.now() - lastRequestTime))) / 1000, intervalInSeconds) + intervalInSeconds
         await bot.sendMessage(chat_id, `La búsqueda ${args[0][0]} fue encolada. Posición en la cola: ${queue_size}. Demora estimada: ${estimated_time} segundos`);
     }
 }
