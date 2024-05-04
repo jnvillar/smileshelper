1c73d3a9cdc35872c48dba73d59ba67ec7bcbd12
Juani <juaninolivillar@gmail.com>
2024-05-04 10:14:05 -0300

    fix

--- clients/smilesClient.js
+++ clients/smilesClient.js
@@ -17,6 +17,7 @@ const flightsHeaders = {
     'Accept-Encoding': 'gzip, deflate, br, zstd'
 }
 
+
 const user_agents = [
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
